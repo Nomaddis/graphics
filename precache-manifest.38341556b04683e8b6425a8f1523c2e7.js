f0b84edc16f53fd0912b25eab48e52424a282c52
Serhii Onishchenko <sonischenko@perpet.io>
2018-11-18 21:53:26 +0200

    Updates

---
+++ precache-manifest.38341556b04683e8b6425a8f1523c2e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72a04dd3b8564dbb84f9",
+    "url": "/graphics/static/js/runtime~main.72a04dd3.js"
+  },
+  {
+    "revision": "e0888e3eb373e3628a3d",
+    "url": "/graphics/static/js/main.e0888e3e.chunk.js"
+  },
+  {
+    "revision": "8f3e7ad3740c78dc9ca9",
+    "url": "/graphics/static/js/1.8f3e7ad3.chunk.js"
+  },
+  {
+    "revision": "e0888e3eb373e3628a3d",
+    "url": "/graphics/static/css/main.8c251b09.chunk.css"
+  },
+  {
+    "revision": "080b409893952b264363e4b70e907bbb",
+    "url": "/graphics/index.html"
+  }
+];
