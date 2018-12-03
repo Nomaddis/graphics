20f1c1a9f0cf28ec5686b6251d925989ecc7da7a
Serhii Onishchenko <sonischenko@perpet.io>
2018-12-03 11:30:18 +0200

    Add footer text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,24 @@
 import React, {Component} from 'react';
-
+import './Footer.css'
 class Footer extends Component {
   render() {
     return (
-      <footer className='footer'>
-        <h3>Footer with contacts</h3>
+      <footer className="footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="footer-inner">
+                <div className="footer-contacts-wrap">
+                  <div><span>Національний університет "Львівська політехніка"</span></div>
+                  <div><span>Інститут телекомунікацій електроніки та електронної техніки</span></div>
+                  <div><span>Кафедра телекомунікацій</span></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
       </footer>
 
 
